b7dbb69a3470739fa91ab13caa7516ca8457fe14
Josué Martínez Fernández <jmartnz.designer@gmail.com>
2022-01-23 00:24:02 +0100

    Add second test

--- src/test/kotlin/PrimeFactorsTest.kt
+++ src/test/kotlin/PrimeFactorsTest.kt
@@ -5,10 +5,18 @@ class PrimeFactorsTest : FreeSpec ({
     "test one" {
         PrimeFactors(1).generate() shouldBe emptyList()
     }
-    
+
+    "test two" {
+        PrimeFactors(2).generate() shouldBe listOf(2)
+    }
+
 })
 
-class PrimeFactors(number: Int) {
-    fun generate(): List<Int> = emptyList()
+class PrimeFactors(private val number: Int) {
+    fun generate(): List<Int> {
+        val primes = mutableListOf<Int>()
+        if (number > 1) primes.add(2)
+        return primes
+    }
 
 }
